86af9b2367abe511d13b3746dbcd6b23305d202e
Prakash Banjade <prakashbanjade191@gmail.com>
2022-11-23 15:26:15 +0545

    1st day

---
+++ SortArray.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+void printArray(int *array, int n);
+
+int main(int argc, char const *argv[])
+{
+  int myArr[10];
+  char position[][5] = {"1st", "2nd", "3rd", "4th", "5th",
+                        "6th", "7th", "8th", "9th", "Last"};
+
+  int arrayLength = 3;
+  for (int i = 0; i < arrayLength; i++)
+  {
+    cout << "Enter " << position[i] << " no.: ";
+    cin >> myArr[i];
+  }
+
+  for (int i = 0; i < arrayLength - 1; i++)
+  {
+    for (int j = 0; j < arrayLength - 1 - i; j++)
+    {
+      if (myArr[j] > myArr[j + 1])
+      {
+        int temp = myArr[j];
+        myArr[j] = myArr[j + 1];
+        myArr[j + 1] = temp;
+      
+      }
+    }
+  }
+
+  cout << endl << "Sorted Array:" << endl;
+  printArray(myArr, arrayLength);
+
+  return 0;
+}
+
+void printArray(int *array, int n)
+{
+  for (int i = 0; i < n; i++)
+  {
+    cout << array[i] << ", ";
+  }
+}
